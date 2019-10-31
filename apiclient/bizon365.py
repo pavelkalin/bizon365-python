267d0a70fbe4b0b282be1de1b3471e202eefdd54
Pavel Kalinichenko <pavel.kalinichenko@gmail.com>
2019-10-31 20:34:37 +0300

    add get viewers of webinars

--- apiclient/bizon365.py
+++ apiclient/bizon365.py
@@ -57,11 +57,57 @@ class Webinars:
 
     def get_subpages(self, skip: int = 0, limit: int = 50):
         """
-        Получение списка страниц регистрации и их рассылок.
+        Получить список страниц регистрации и их рассылок.
+        https://blog.bizon365.ru/api/v1/webinars/subpages/#getsubpages
         :param skip:  skip — количество записей, которые нужно пропустить [необязательный параметр]
         :param limit: limit — количество записей, которые нужно получить [необязательный параметр]
         :return: JSON response
         """
         url = 'webinars/subpages/getSubpages?skip={skip}&limit={limit}'.format(skip=skip, limit=limit)
+        r = self._client.get(url)
+        return r
+
+    def get_list(self, skip: int = 0, limit: int = 50, live: int = 1, auto: int = 1):
+        """
+        Полученить список доступных отчетов
+        https://blog.bizon365.ru/api/v1/webinars/reports/#getlist
+        :param skip: пропустить указанное число записей
+        :param limit: ограничить количество записей. Не более 100.
+        :param live: искать среди живых вебинаров
+        :param auto: искать среди автовебинаров
+        :return: JSON {
+                        name  (строка) идентификатор комнаты. По этому параметру можно определить нужный webinarID.
+                        webinarId  (строка) идентификатор вебинара. Этот id используется для получения отчета по вебинару.
+                        type (строка) тип вебинара: LiveWebinars или AutoWebinars
+                        created (строка, дата в формате ISO) дата создания записи
+                        count1 (число) количество участников вебинара
+                        count2 (число) длительность вебинара в минутах
+                    }
+        """
+        url = 'webinars/reports/getlist?skip={skip}&limit={limit}&LiveWebinars={live}&AutoWebinars={auto}'.format(
+            skip=skip, limit=limit, live=live, auto=auto)
+        r = self._client.get(url)
+        return r
+
+    def get_webinar_report(self, webinar_id: str):
+        """
+        Получить отчет по конкретному вебинару
+        :param webinar_id: идентификатор вебинара
+        :return: JSON
+        """
+        url = 'webinars/reports/get?webinarId={webinar_id}'.format(webinar_id=webinar_id)
+        r = self._client.get(url)
+        return r
+
+    def get_webinar_viewers(self, webinar_id: str, skip: int = 0, limit: int = 50):
+        """
+        Получить список зрителей по конкретному вебинару
+        :param webinar_id: идентификатор вебинара
+        :param skip: пропустить указанное число записей
+        :param limit: ограничить количество записей. Не более 1000.
+        :return: JSON
+        """
+        url = 'webinars/reports/getviewers?webinarId={webinar_id}&skip={skip}&limit={limit}'.format(
+            webinar_id=webinar_id, skip=skip, limit=limit)
         r = self._client.get(url)
         return r
--- examples/initial.py
+++ examples/initial.py
@@ -6,5 +6,15 @@ API_KEY = os.getenv('API_KEY', None)
 
 bizon = Webinars(api_key=API_KEY)
 
-subpages = bizon.get_subpages()
-print('List of all subpages is: \n {}'.format(subpages))
+# subpages = bizon.get_subpages()
+# print('List of all subpages is: \n {}'.format(subpages))
+#
+# reports = bizon.get_list(live=0, auto=1)
+# print('List of all reports is:')
+# print(json.dumps(reports, indent=4, sort_keys=True))
+
+# webinar_report = bizon.get_webinar_report('XXXXX')
+# print(json.dumps(webinar_report, indent=4, sort_keys=True))
+
+webinar_report = bizon.get_webinar_viewers('XXXX')
+print(json.dumps(webinar_report, indent=4, sort_keys=True))
